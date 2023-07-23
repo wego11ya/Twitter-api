a28517d137fbdaea81f3ee5c48b7bc77ba291f82
wego11ya <wego11ya@gmail.com>
2023-07-23 11:04:20 +0800

    feat: initialize route

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const helpers = require('./_helpers');
+const express = require("express");
+const helpers = require("./_helpers");
+const routes = require("./routes");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+}
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-module.exports = app
+module.exports = app;
