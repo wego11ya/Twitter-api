945d4b926dfc5179ade7d65fbd5788d174d78da0
wego11ya <wego11ya@gmail.com>
2023-08-11 09:25:01 +0800

    feat: add tweetController.postReply, getReplies, addLike, removeLike

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -103,5 +103,75 @@ const tweetController = {
       next(error);
     }
   },
+  postReply: async (req, res, next) => {
+    try {
+      const currentUser = getUser(req);
+      const tweetId = Number(req.params.tweet_id);
+      const comment = req.body.comment.trim();
+      const tweet = await Tweet.findOne({ where: { id: tweetId } });
+
+      if (!tweet) throw newError(404, "該貼文不存在");
+      if (!comment) throw newError(400, "內容不可空白");
+      if (comment.length > 140) throw newError(400, "字數不可超過140字");
+
+      const replyInfo = await Reply.create({
+        UserId: currentUser.id,
+        TweetId: tweetId,
+        comment,
+      });
+      res.json(replyInfo);
+    } catch (error) {
+      next(error);
+    }
+  },
+  getReplies: async (req, res, next) => {
+    try {
+      const tweetId = Number(req.params.tweet_id);
+      const tweet = await Tweet.findByPk(tweetId);
+      if (!tweet) throw newError(404, "該貼文不存在");
+      const repliesInfo = await Reply.findAll({
+        where: { TweetId: tweetId },
+        attributes: ["id", "comment", "createdAt"],
+        include: [
+          { model: User, attributes: ["id", "account", "name", "avatar"] },
+        ],
+        order: [["createdAt", "DESC"]],
+      });
+      res.json(repliesInfo);
+    } catch (error) {
+      next(error);
+    }
+  },
+  addLike: async (req, res, next) => {
+    try {
+      const currentUser = getUser(req);
+      const tweetId = Number(req.params.id);
+      const [tweet, like] = await Promise.all([
+        Tweet.findByPk(tweetId),
+        Like.findOne({ where: { UserId: currentUser.id, TweetId: tweetId } }),
+      ]);
+      if (!tweet) throw newError(404, "該貼文不存在");
+      if (like) throw newError(400, "已按過讚");
+      const likeInfo = await Like.create({
+        UserId: currentUser.id,
+        TweetId: tweetId,
+      });
+      res.json(likeInfo);
+    } catch (error) {
+      next(error);
+    }
+  },
+  removeLike: async (req, res, next) => {
+    try {
+      const currentUser = getUser(req);
+      const tweetId = Number(req.params.id);
+      const unLike = await Like.destroy({
+        where: { UserId: currentUser.id, TweetId: tweetId },
+      });
+      res.json({ deleted: unLike > 0 });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 module.exports = tweetController;
--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const tweetController = require("../../controllers/tweet-controller");
 
-router.get("/", tweetController.getTweets);
+router.post("/:tweet_id/replies", tweetController.postReply);
+router.get("/:tweet_id/replies", tweetController.getReplies);
+router.post("/:id/like", tweetController.addLike);
+router.post("/:id/unlike", tweetController.removeLike);
 router.get("/:tweet_id", tweetController.getTweet);
+router.get("/", tweetController.getTweets);
 router.post("/", tweetController.postTweet);
 
 module.exports = router;
